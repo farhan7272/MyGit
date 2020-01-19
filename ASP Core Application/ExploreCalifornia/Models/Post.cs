0fd52fefd9855e5dbac6ec0dd3ade10fe3fd9404
farhan7272 <farhan7272@users.noreply.github.com>
2020-01-19 08:32:38 -0600

    Validation

--- ASP Core Application/ExploreCalifornia/Models/Post.cs
+++ ASP Core Application/ExploreCalifornia/Models/Post.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,13 @@ namespace ExploreCalifornia.Models
 {
     public class Post
     {
+        [Required]
+        [StringLength(100,MinimumLength = 5,ErrorMessage ="Title name must be between 5 to 100")]
         public string Title { get; set; }
         public string Author { get; set; }
+        
+        [Required]
+        [MinLength(100,ErrorMessage ="Minimum length of blog should be 100 characters")]
         public string Body { get; set; }
         public DateTime Posted { get; set; }
     }
