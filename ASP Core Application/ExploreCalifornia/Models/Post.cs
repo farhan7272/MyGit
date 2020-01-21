009c14b16b7da3fd8fd675a0a22c9a277f7e272f
farhan7272 <farhan7272@users.noreply.github.com>
2020-01-20 18:44:10 -0600

    Configuring DB

--- ASP Core Application/ExploreCalifornia/Models/Post.cs
+++ ASP Core Application/ExploreCalifornia/Models/Post.cs
@@ -2,12 +2,30 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ExploreCalifornia.Models
 {
     public class Post
     {
+        
+        private string _key;
+
+        public string Key
+        {
+            get
+            {
+                if (_key == null)
+                {
+                    _key = Regex.Replace(Title.ToLower(), "[^a-z0-9]", "-");
+                }
+                return _key;
+            }
+            set { _key = value; }
+        }
+
+
         [Required]
         [StringLength(100,MinimumLength = 5,ErrorMessage ="Title name must be between 5 to 100")]
         public string Title { get; set; }
--- ASP Core Application/ExploreCalifornia/Startup.cs
+++ ASP Core Application/ExploreCalifornia/Startup.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using ExploreCalifornia.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace ExploreCalifornia
 {
@@ -24,6 +22,11 @@ namespace ExploreCalifornia
             services.AddTransient<FeatureToggle>(x => new FeatureToggle
             {
                 EnableDevelopersException = configuration.GetValue<bool>("FeaturesToggles:EnableDevelopersException")
+            });
+
+            services.AddDbContext<BlogDataContext>(options => {
+                var connectionString = configuration.GetConnectionString("BlogDbConnection");
+                options.UseSqlServer(connectionString);
             });
             services.AddMvc(option => option.EnableEndpointRouting = false);
         }
