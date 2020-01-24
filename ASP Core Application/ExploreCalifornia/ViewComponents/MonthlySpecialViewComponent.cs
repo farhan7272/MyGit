75995befc795ff321f6e3335596179603ed73d99
farhan7272 <farhan7272@users.noreply.github.com>
2020-01-24 17:04:12 -0600

    View Component

---
+++ ASP Core Application/ExploreCalifornia/ViewComponents/MonthlySpecialViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExploreCalifornia.Models;
+
+namespace ExploreCalifornia.ViewComponents
+{
+    [ViewComponent]
+    public class MonthlySpecialViewComponent : ViewComponent
+    {
+        private readonly BlogDataContext _db;
+
+        public MonthlySpecialViewComponent(BlogDataContext db)
+        {
+            _db = db;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var specials = _db.MonthlySpecials.ToArray();
+            return View(specials);
+        }
+    }
+}
